51a27f41f08272a2899943371cd7c989133e42dd
Candice Yu <candiceyu@CandicedeMacBook-Pro.local>
2016-05-25 01:27:27 +0800

    add class

---
+++ app/src/main/java/com/app/candiceyu/coolweather/db/CoolWeatherOpenHelper.java
@@ -0,0 +1,24 @@
+package com.app.candiceyu.coolweather.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by candiceyu on 16/5/25.
+ */
+public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
+
+    public CoolWeatherOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
